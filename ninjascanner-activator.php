1a18e7033c380d34519973cb0efdc17ffae0c54d
mohamedhk2 <mohamedhk2@yahoo.fr>
2023-10-19 00:06:48 +0100

    refactor functions.php

--- ninjascanner-activator.php
+++ ninjascanner-activator.php
@@ -5,21 +5,22 @@
  * Plugin Name:       NinjaScanner Activator
  * Plugin URI:        https://github.com/wp-activators/ninjascanner-activator
  * Description:       NinjaScanner Plugin Activator
- * Version:           1.1.0
- * Requires at least: 3.1.0
+ * Version:           1.2.0
+ * Requires at least: 5.9.0
  * Requires PHP:      7.2
  * Author:            mohamedhk2
  * Author URI:        https://github.com/mohamedhk2
  **/
 
 defined( 'ABSPATH' ) || exit;
-const NINJA_SCANNER_ACTIVATOR_NAME   = 'NinjaScanner Activator';
-const NINJA_SCANNER_ACTIVATOR_DOMAIN = 'ninjascanner-activator';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+$NINJA_SCANNER_ACTIVATOR_NAME   = 'NinjaScanner Activator';
+$NINJA_SCANNER_ACTIVATOR_DOMAIN = 'ninjascanner-activator';
+$functions                      = require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+extract( $functions );
 if (
-	activator_admin_notice_ignored()
-	|| activator_admin_notice_plugin_install( 'ninjascanner/index.php', 'ninjascanner', 'NinjaScanner', NINJA_SCANNER_ACTIVATOR_NAME, NINJA_SCANNER_ACTIVATOR_DOMAIN )
-	|| activator_admin_notice_plugin_activate( 'ninjascanner/index.php', NINJA_SCANNER_ACTIVATOR_NAME, NINJA_SCANNER_ACTIVATOR_DOMAIN )
+	$activator_admin_notice_ignored()
+	|| $activator_admin_notice_plugin_install( 'ninjascanner/index.php', 'ninjascanner', 'NinjaScanner', $NINJA_SCANNER_ACTIVATOR_NAME, $NINJA_SCANNER_ACTIVATOR_DOMAIN )
+	|| $activator_admin_notice_plugin_activate( 'ninjascanner/index.php', $NINJA_SCANNER_ACTIVATOR_NAME, $NINJA_SCANNER_ACTIVATOR_DOMAIN )
 ) {
 	return;
 }
@@ -29,13 +30,13 @@ if ( $nscan_options ) {
 	$nscan_options  ['key'] = 'free4all-free4all-free4all-free4all-free4all';
 	update_option( 'nscan_options', $nscan_options );
 }
-add_filter( 'pre_http_request', function ( $pre, $parsed_args, $url ) {
+add_filter( 'pre_http_request', function ( $pre, $parsed_args, $url ) use ( $activator_json_response ) {
 	$target = defined( 'NSCAN_SIGNATURES_URL' ) ? NSCAN_SIGNATURES_URL : 'https://ninjascanner.nintechnet.com/index.php';
 	switch ( $url ) {
 		case $target:
 			switch ( $parsed_args['body']['action'] ?? null ) {
 				case 'check_license':
-					return activator_json_response( [
+					return $activator_json_response( [
 						'checked' => true,
 						'exp'     => date( 'Y-m-d', strtotime( '+1000 year' ) ),
 					] );
